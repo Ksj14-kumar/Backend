1599263eced289b6f38d806d90d0a5c9346de150
Kumar <lionmassymassy@gmail.com>
2022-07-12 14:54:57 +0530

    add

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const server = http.createServer(app)
 const { Server } = require("socket.io")
 const io = new Server(server, {
 
-    path: process.env.PATH,
+    path:"/collegezone",
     // transports: [ "websocket"],
     cors: {
         origin: process.env.CLIENT_URL,
